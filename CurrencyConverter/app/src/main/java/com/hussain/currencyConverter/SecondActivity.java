8b5af9529ce465932451bc7854d07bd0c6591939
Mhussain4 <49729886+Mhussain4@users.noreply.github.com>
2021-08-25 18:58:18 +0500

    Project

---
+++ CurrencyConverter/app/src/main/java/com/hussain/currencyConverter/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.hussain.currencyConverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.firstapplication.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
